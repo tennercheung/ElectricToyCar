dcefd57cdbf4c6bea93fcc977e4e8ff2ee8a4be8
tennercheung <tennercheung@gmail.com>
2019-05-06 22:12:12 -0400

    updated prototype 1 code

--- ProtoV1/ProtoV1.ino
+++ ProtoV1/ProtoV1.ino
@@ -1,6 +1,7 @@
 /* 
   wasd = 20DF02FD, 20DFE01F, 20DF609F, 20DF827D
   + , - (pwm) = 20DF40BF, 20DFC03F
+  
 
 M1:
 IN1 = 8, IN2 = 9
@@ -25,32 +26,39 @@ const int RECV_PIN = 2;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+int in_speed = 0;
+//boolean B1 = digitalRead(B1_PIN);
+//boolean B2 = digitalRead(B2_PIN);
+//boolean B3 = digitalRead(B1_PIN);
+//boolean B4 = digitalRead(B2_PIN);
 
+char fwd[8] = "Forward";
+char bwd[9] = "Backward";
 
-void change_direc (bool direct, int motor_num) {
-  if ((direct == HIGH or direct == LOW) && motor_num == 1) {
+void change_direc (bool direct, int motor_num_) {
+  if ((direct == HIGH or direct == LOW) && motor_num_ == 1) {
     digitalWrite(IN1, direct);
     digitalWrite(IN2, !direct);
   }
 
-  else if ((direct == HIGH or direct == LOW) && motor_num == 2) {
+  else if ((direct == HIGH or direct == LOW) && motor_num_ == 2) {
     digitalWrite(IN3, direct);
     digitalWrite(IN4, !direct);
   }
 }
 
-void run_motors (int speed, char direc[], int movement) {
+void run_motors (int speed, char direc[], int motor_num) {
   analogWrite(EN1, speed);
   analogWrite(EN2, speed);
 
-  if (movement == 25) {
+  if (direc == "Forward") {
     Serial.println("  fwd active  ");
-    change_direc(HIGH, 1); //forward
+    change_direc(HIGH, motor_num); //forward
   }
 
-  else if (movement == 50) {
+  else if (direc == "Backward") {
     Serial.println("  bwd active  ");
-    change_direc(LOW, 1); //backward
+    change_direc(LOW, motor_num); //backward
 
   }
 
@@ -68,12 +76,33 @@ void setup(){
 }
 
 void loop(){
-  if (irrecv.decode(&results)){
-        Serial.println(results.value, HEX);
-        irrecv.resume();
-        delay(250);
+//  if (irrecv.decode(&results)){
+//      Serial.println(results.value, HEX);
+//      irrecv.resume();
+//      delay(250);
+//  }
+   
+  int speed = in_speed;
+  if ((in_speed <= 255) && (in_speed >= 0) ){
+    if (results.value == 0x20DF40BF){
+      in_speed + 5 ;
+    }
+    else if (results.value == 0x20DFC03F){
+      in_speed - 5 ;
+    }
+  }
+  
+  if (results.value == 0x20DF02FD) {
+    run_motors(in_speed, fwd, 1);
+  }
+  else if (results.value == 0x20DF609F) {
+    run_motors(in_speed, bwd, 1);
   }
 
+  20DFE01F
+
+  20DF827D
 
   
+  irrecv.resume();
 }
