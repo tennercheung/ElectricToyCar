da897c5c5b31d965e6cac1271f1e775ecfdda5ba
tennercheung <tennercheung@gmail.com>
2019-06-16 18:54:26 -0400

    added template buttons

--- ProtoV2/ProtoV2.ino
+++ ProtoV2/ProtoV2.ino
@@ -5,26 +5,26 @@
 
   M1:
   IN1 = 8, IN2 = 9
-  EN1 = 10
+  EN1 = 6
   M2:
   EN2 = 11
-  IN3 = 12, IN4 = 7
+  IN3 = 10, IN4 = 7
 */
 
 //M1:
 #define IN1 8
 #define IN2 9
-#define EN1 10
+#define EN1 6
 
 //M2:
-#define IN3 12
+#define IN3 10
 #define IN4 7
 #define EN2 11
 
-#define B1_PIN 3
-#define B2_PIN 4
+#define BU1 3
+#define BU2 4
 
-#define STEER 6
+#define STEER 5
 
 #define FORWARD 1
 #define BACKWARD 2
@@ -33,10 +33,13 @@
 #define CH2 A2
 #define POTPIN A0
 
-int speed = 255;
+int speed = 75;
 
 int ch1;
 int ch2;
+
+boolean Bu1 = digitalRead(BU1);
+boolean Bu2 = digitalRead(BU2);
 
 int steerval;    // variable to read the value from the analog pin
 
@@ -67,21 +70,11 @@ void run_motors (int speed, int direc) {
     change_direc(LOW); //backward
 
   }
-
 }
 
 void motor_off () {
-
-
   analogWrite(EN1, 0);
   analogWrite(EN2, 0);
-
-}
-
-void servo_time () {
-  for ( int a = 0; a < 255; ++a ) {
-    // waits for the servo to get there
-  }
 }
 
 void setup() {
@@ -98,14 +91,16 @@ void setup() {
   pinMode(CH1, INPUT);
   pinMode(CH2, INPUT);
 
-  steer.attach(STEER);  // attaches the servo on pin 9 to the servo object
+  pinMode(BU1, INPUT_PULLUP);
+  pinMode(BU2, INPUT_PULLUP);
+
   steer.write(90);
+  steer.attach(STEER);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
 
-  boolean Bu1 = digitalRead(B1_PIN);
-  boolean Bu2 = digitalRead(B2_PIN);
+
 
   ch1 = pulseIn(CH1, HIGH, 25000);
   delay(12);  ch2 = pulseIn(CH2, HIGH, 25000);
@@ -116,27 +111,30 @@ void loop() {
   //  Serial.print("  Bu2:");Serial.println(Bu2);
 
   //  Serial.print("Chan 1:"); Serial.print(ch1);
-  Serial.print("  Chan 2:"); Serial.println(ch2);
+  //  Serial.print("  Chan 2:"); Serial.println(ch2);
   //  Serial.print("angle"); Serial.println(steerval);
 
   if (ch2 > 2010) {
     run_motors(speed, FORWARD);
   }
-  //    else if (Bu1 == HIGH) { // run fwd
-  //      run_motors(speed, FORWARD);
-  //    }
+  else if ( (Bu1 == LOW) and (Bu2 == HIGH) ) { // run fwd
+    run_motors(speed, FORWARD);
+  }
 
-  else if (ch2 < 1400 and ch2 != 0) {
+
+  if (ch2 < 1400 and ch2 != 0) {
     run_motors(speed, BACKWARD);
   }
-  //    else if (Bu2 == HIGH) { // run back
-  //      run_motors(speed, BACKWARD);
-  //    }
+  else if ( (Bu2 == LOW) and (Bu1 == HIGH) ) { // run back
+    run_motors(speed, BACKWARD);
+  }
+
   else { //run idle
     motor_off();
   }
 
   if (( (ch1 < 1677) or (ch1 > 1725) ) and (ch1 != 0)) {
+
     if (ch1 > 1810) {
       steer.write(0);
     }
