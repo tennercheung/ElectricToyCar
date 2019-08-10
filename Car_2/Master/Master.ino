1d998109ffdd8ac54051caa38d7042d507dfd043
Tenner <tennercheung@gmail.com>
2019-08-10 15:33:53 -0400

    testing with hbridge on protocar

--- Car_2/Master/Master.ino
+++ Car_2/Master/Master.ino
@@ -1,50 +1,3 @@
-//
-//#define analogPin A0
-//int raw = 0;
-//int Vin = 5;
-//float Vout = 0;
-//float R1 = 12000;
-//float R2 = 0;
-//float buffer = 0;
-//
-//void setup()
-//{
-//  Serial.begin(9600);
-//  pinMode(2, OUTPUT);
-//  pinMode(3, OUTPUT);
-//  pinMode(4, OUTPUT);
-//}
-//
-//void loop()
-//{
-//  raw = analogRead(analogPin);
-//  if (raw)
-//  {
-//    buffer = raw * Vin;
-//    Vout = (buffer) / 1024.0;
-//    buffer = (Vin / Vout) - 1;
-//    R2 = R1 * buffer;
-////    Serial.print("Vout: ");
-////    Serial.println(Vout);
-////    Serial.print("R2: ");
-////    Serial.println(R2);
-//  }
-//  if (R2 > 5600 and R2 < 6600) {
-//    analogWrite(3, 0);
-//    digitalWrite(2, HIGH);
-//    digitalWrite(4, LOW);
-//  }
-//  else if (R2 < 5600 and R2 > 4200) {
-//    analogWrite(3, 0);
-//    digitalWrite(2, LOW);
-//    digitalWrite(4, HIGH);
-//  }
-//
-//  else {
-//    analogWrite(3, 255);
-//  }
-//}
-
 
 /*
 
@@ -66,10 +19,7 @@
 #define IN4 7
 #define EN2 11
 
-#define steerOUT 5
 
-#define steerPin A0
-#define CH1 A1
 #define CH2 A2
 
 #define FORWARD 1
@@ -82,16 +32,9 @@
 #define RIGHTBWD 7
 int speed = 50;
 int turnDirection;
-int ch1;
+
 int ch2;
 
-int steerval;
-int raw = 0;
-int Vin = 5;
-float Vout = 0;
-float R1 = (12*1000);
-float R2 = 0;
-float buffer = 0;
 
 void change_direc (bool direct) {
   if ((direct == HIGH || direct == LOW)) {
@@ -163,65 +106,18 @@ void setup()
   pinMode(IN4, OUTPUT);
   pinMode(EN2, OUTPUT);
 
-  pinMode(CH1, INPUT);
+
   pinMode(CH2, INPUT);
 
-  pinMode(steerOUT, OUTPUT);
+
 }
 
 void loop() {
-  ch1 = pulseIn(CH1, HIGH, 25000);
-  delay(12);  ch2 = pulseIn(CH2, HIGH, 25000);
-
-  int steermap = map((analogRead(steerPin)), 0, 1023, 0, 180);
-  steerval = analogRead(steerPin);
-
-  raw = analogRead(steerPin);
+  ch2 = pulseIn(CH2, HIGH, 25000);
+  Serial.println(ch2);
 
 
-
-//  if (raw) {
-//    buffer = raw * Vin;
-//    Vout = (buffer) / 1024.0;
-//    buffer = (Vin / Vout) - 1;
-//    R2 = R1 * buffer;
-//  }
-
-  if (ch1 > 2000) {
-    analogWrite(steerOUT, 255);
-    turnDirection = RIGHT;
-  }
-  else if (ch1 < 1470 and ch1 != 0) {
-    analogWrite(steerOUT, 0);
-    turnDirection = LEFT;
-  }
-//  else if (R2 > 6600){
-//    analogWrite(steerOUT, 255);
-//  }
-//  else if (R2 < 4200){
-//    analogWrite(steerOUT, 0);
-//  }
-  else {
-    analogWrite(steerOUT, 120);
-    turnDirection = CENTRE;
-  }
-//
-//  if (R2 >= 5600 and R2 < 6600) {
-//    analogWrite(3, 0);
-//    digitalWrite(2, HIGH);
-//    digitalWrite(4, LOW);
-//  }
-//  else if (R2 < 5600 and R2 > 4200) {
-//    analogWrite(3, 0);
-//    digitalWrite(2, LOW);
-//    digitalWrite(4, HIGH);
-//  }
-//
-//  else {
-//    analogWrite(3, 255);
-//  }
-
-    if (ch2 > 2010) {
+    if (ch2 >= 1450) {
       if (turnDirection == LEFT){
         run_motors(speed, LEFT);
       }
@@ -232,7 +128,7 @@ void loop() {
         run_motors(speed, FORWARD);
       }
   }
-  else if (ch2 < 1400 and ch2 != 0) {
+  else if (ch2 <= 1190 and ch2 != 0) {
       if (turnDirection == LEFT){
         run_motors(speed, RIGHTBWD);
       }
